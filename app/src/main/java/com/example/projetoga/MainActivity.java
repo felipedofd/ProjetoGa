abaaf6609e8b7eb097c7725a52d2e9c11a5c94b4
Felipe <felipedofd@hotmail.com>
2021-06-05 19:24:58 -0300

    Novos metodos

--- app/src/main/java/com/example/projetoga/MainActivity.java
+++ app/src/main/java/com/example/projetoga/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +13,9 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
+import coil.Coil;
+import coil.ImageLoader;
+import coil.request.ImageRequest;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,6 +23,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
  public class MainActivity extends Activity {
+
+     private String apiKey = "AE2DC846D2E477E97A841CED5E0ED908";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,40 +37,88 @@ import retrofit2.converter.gson.GsonConverterFactory;
         TextView deaths = findViewById(R.id.total_deaths);
         TextView kills = findViewById(R.id.total_kills);
         TextView wins = findViewById(R.id.total_wins);
+        ImageView avatar = findViewById(R.id.user_image);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.steampowered.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        SteamService service = retrofit.create(SteamService.class);
+        SteamService servico = retrofit.create(SteamService.class);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        // Clique no botao
+        button.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-               ArrayList<String> listIds = new ArrayList<String>();
-               listIds.add("76561198002721112");
+            public void onClick(View view) {
+                getStats(servico, pid.getText().toString(),id.getText().toString(),kills, deaths, wins);
+                getUserInfo(servico, id.getText().toString(), avatar);
 
-                service.getPlayerSummaries( "AE2DC846D2E477E97A841CED5E0ED908", listIds).enqueue(new Callback<ResponseSteam3>() {
+            }
+        });
+
+
+    }
+
+
+
+
+    void getUserInfo(SteamService service, String id, ImageView avatar2){
+        ArrayList<String> listaId = new ArrayList<>();
+        listaId.add(id);
+        service.getPlayerSummaries(apiKey, listaId).enqueue(new Callback<ResponseSteam3>() {
+            @Override
+            public void onResponse(Call<ResponseSteam3> call, Response<ResponseSteam3> response) {
+                if(response.isSuccessful()){
+                    String avatarUrl = response.body().getResponse().getPlayerList().get(0).getAvatar();
+
+                    ImageLoader imageLoader = Coil.imageLoader(getApplicationContext());
+                    ImageRequest request = new ImageRequest.Builder(getApplicationContext())
+                            .data(avatarUrl)
+                            .crossfade(true)
+                            .target(avatar2)
+                            .build();
+                    imageLoader.enqueue(request);
+                }
+                else{
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseSteam3> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    void getStats(SteamService service, String pid, String id, TextView kills, TextView deaths, TextView wins ){
+        // Serviço passado pid, key, id
+        service.getUserStatsForGame(pid, apiKey, id )
+                .enqueue(new Callback<ResponseSteam>() {
                     @Override
-                    public void onResponse(Call<ResponseSteam3> call, Response<ResponseSteam3> response) {
-                        kills.setText("Id igual a: " + response.body().getResponse().getPlayerList().get(0).getPersonaName());
-//                        kills.setText("Quantidade de jogadores abatidos: " + response.body().playerStats.stats.get(0).getValue());
-//                        deaths.setText("Quantidades de Mortes: " + response.body().playerStats.stats.get(1).getValue());
-//                        wins.setText("Quantidade de Vitorias: " + response.body().playerStats.stats.get(5).getValue());
+                    public void onResponse(Call<ResponseSteam> call, Response<ResponseSteam> response) {
+                        // Resposta
+                        if (response.isSuccessful()) {
+                            kills.setText("Você matou: "+ response.body().getPlayerStats().getStats().get(0).value);
+                            deaths.setText("Você morreu: "+ response.body().getPlayerStats().getStats().get(1).value);
+                            wins.setText("Você ganhou: "+ response.body().getPlayerStats().getStats().get(5).value);
+
+                        } else {
+                            deaths.setText("Erro");
+                            kills.setText("Erro");
+                            wins.setText("Erro");
+                        }
                     }
 
                     @Override
-                    public void onFailure(Call<ResponseSteam3> call, Throwable t) {
+                    public void onFailure(Call<ResponseSteam> call, Throwable t) {
+                        // Erro
                         deaths.setText("Erro");
                         kills.setText("Erro");
                         wins.setText("Erro");
                     }
                 });
-            }
-        });
-
     }
 }
-
 
--- app/src/main/java/com/example/projetoga/Player.java
+++ app/src/main/java/com/example/projetoga/Player.java
@@ -7,6 +7,9 @@ public class Player {
     @SerializedName("personaname")
     private String personaName;
 
+    @SerializedName("avatarfull")
+    private String avatarFull;
+
     public Player(String personaName) {
         this.personaName = personaName;
     }
@@ -16,7 +19,21 @@ public class Player {
     }
 
     public void setPersonaName(String personaName) {
-        this.personaName = personaName;}
+        this.personaName = personaName;
+    }
+
+    public String getAvatar() {
+        return avatarFull;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatarFull = avatar;
+    }
+
+    public void setPersonaName(String personaName, String avatar) {
+        this.personaName = personaName;
+        this.avatarFull = avatar;
+    }
 
 }
 
